2b55574767267d6b01199775c09b36ca6b7db707
carlosgarciara <carlos.garcia.ra@outlook.com>
2023-07-27 02:55:19 -0600

    template

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,3 @@
 import authRoutes from './auth';
-import programmingLanguagesRoutes from './programmingLanguages';
-import profileRoutes from './profile';
-import skillsRoutes from './skills';
-import ProjectsRoutes from './projects';
 
-export const routes = [...authRoutes, ...programmingLanguagesRoutes, ...profileRoutes, ...skillsRoutes, ...ProjectsRoutes];
+export const routes = [...authRoutes];
